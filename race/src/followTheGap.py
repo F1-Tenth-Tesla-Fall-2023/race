9e573f4e0f2fcf92cc083d23ca67ef11281f52b6
EsamA123 <112518138+EsamA123@users.noreply.github.com>
2023-11-08 17:06:28 -0500

    Updated followTheGap.py with new logic

--- race/src/followTheGap.py
+++ race/src/followTheGap.py
@@ -1,105 +1,207 @@
-import rospy
+#!/usr/bin/env python
 import math
-from sensor_msgs.msg import LaserScan
+import rospy
 from ackermann_msgs.msg import AckermannDrive
+from sensor_msgs.msg import LaserScan
+import numpy as np
 
-# ROS publisher for controlling the car's movement
-command_pub = rospy.Publisher('/car_8/ackermann_cmd', AckermannDrive, queue_size=1)
+global car_width # width of the car, used to calculate the number of samples to cover half the car width
+global threshold # threshold for a disparity
+global limit_threshold # threshold that when hit the car will go towards center of gap
+global tolerance # tolerance for the number of samples to cover half the car width
 
-# Constants
-threshold = 0.2  # Threshold for identifying disparities (adjust as needed)
-car_width = 0.5  # Width of the car in meters
-tolerance = 0.1  # Tolerance for gap calculation (adjust as needed)
+command_pub = rospy.Publisher('/car_8/offboard/command', AckermannDrive, queue_size = 1)
 
-# Function to calculate the number of LIDAR samples needed
-def calculate_samples_needed(distance, angleInc):
-    alpha = math.atan((car_width / 2 + tolerance) / distance)
-    num_samples_needed = int(math.ceil(alpha / angleInc))
-    return num_samples_needed
+disparity_pub = rospy.Publisher('/car_8/disparity', LaserScan, queue_size = 1)
 
-def process_lidar_scan(fullRange, angleInc, angleMin):
-    # Identify and process disparities
-    for i in range(1, len(fullRange)):
-        # Calculate the difference between subsequent points
-        disparity = abs(fullRange[i] - fullRange[i - 1])
+def calculateSamplesToCoverHalfCarWidth(distance1, distance2, car_width, angle_increment):
 
-        # Check if the disparity is larger than the threshold
-        if disparity > threshold:
-            # Determine the closer distance in the disparity
-            desired_distance = min(fullRange[i], fullRange[i-1]) 
-            # Calculate the number of LIDAR samples needed at closer distance
-            num_samples_needed = calculate_samples_needed(desired_distance, angleInc)
+    global tolerance
 
-            # Overwriting the needed number of samples with the smaller value
-            if desired_distance == fullRange[i-1]:
-                for j in range(i, i+num_samples_needed):
-                    fullRange[j] =  min(fullRange[j],fullRange[i-1])
+    # Calculate the span across the car that each lidar sample covers
+    # Adding the spans across the car width due to two consecutive readings
+    span_per_increment = math.sin(angle_increment) * distance1 + math.sin(angle_increment) * distance2
+
+    # Calculate the number of samples required to cover half the car's width plus a tolerance
+    num_samples = (car_width / 2 * (1 + tolerance)) / span_per_increment
+
+    # Return the ceiling of num_samples because we can't have a fraction of a sample and we need at least this many
+    return int(math.ceil(num_samples))
+
+def extendDisparities(data):
+
+    global car_width
+    global threshold
+
+    angle_increment = data.angle_increment
+    ranges = data.ranges
+
+    new_ranges = list(ranges)
+    n = len(ranges)
+
+    # Find the disparities, where two subsequent points are further apart than the threshold
+    for i in range(n - 1):
+
+        # dismiss if either of the points are inf, or less than car_width
+        if np.isinf(ranges[i]):
+            new_ranges[i] = 0
+            continue
+        if np.isinf(ranges[i + 1]):
+            new_ranges[i + 1] = 0
+            continue
+        
+        # maybe help with corner detection
+        # if ranges[i] < car_width or ranges[i + 1] < car_width:
+        #     new_ranges[i] = 0
+        #     continue
+
+        if abs(ranges[i] - ranges[i + 1]) > threshold:
+            closer_distance = min(ranges[i], ranges[i + 1])
+            samples_to_update = calculateSamplesToCoverHalfCarWidth(ranges[i], ranges[i + 1], car_width, angle_increment)
+            
+            # For each pair of pounts in a disparity,
+            # - calculate the number of lidar samples to cover half the width of the car, plus tolerance
+            # - for the side that is further, set that number of samples to the closer distance - but don't overwrite any closer samples
+            if ranges[i] > ranges[i + 1]:
+                # Extend to the left
+                for j in range(i, max(i - samples_to_update, -1), -1):
+                    if new_ranges[j] > closer_distance:
+                        new_ranges[j] = closer_distance
+                        # new_ranges[j] = 0
             else:
-                for j in range(i-1, i-num_samples_needed-1, -1):
-                    fullRange[j] =  min(fullRange[j],fullRange[i])
+                # Extend to the right
+                for j in range(i + 1, min(i + 1 + samples_to_update, n)):
+                    if new_ranges[j] > closer_distance:
+                        new_ranges[j] = closer_distance
+                        # new_ranges[j] = 0
+    
+    return new_ranges
 
-    # Find the index in the range coorelated to the direct left and right of the car
-    leftAngle = math.radians(0) - (math.pi / 2)
-    rightAngle = math.radians(180) - (math.pi / 2)
-    leftIndex = int((leftAngle - angleMin) / angleInc)
-    rightIndex = int((rightAngle - angleMin) / angleInc)
-    filteredDistances = fullRange[leftIndex:rightIndex+1]
-    #Return the filteredRanges after processing disparities and limiting the range
-    return filteredDistances
-
-def findGap(scan):
-
-    proccessedRanges = process_lidar_scan(scan.ranges, scan.angle_increment, scan.angle_min)
-
-     # Find the closest point 
+def createBubble(scans):
+    ranges = list(scans)
+    n = len(ranges)
     index = 0
     close_distance = 9999
-    for i in range(len(proccessedRanges)):
-        if proccessedRanges[i] > 0.05 and proccessedRanges[i] < close_distance:
+    for i in range(len(ranges)):
+        if ranges[i] > 0.05 and ranges[i] < close_distance:
             index = i 
-            close_distance = proccessedRanges[i]
-    close_angle = math.atan((car_width / 2 + tolerance) / close_distance)
-    
-    # Set the bubble to 0
-    bubble_samples_needed = calculate_samples_needed(close_distance, scan.angle_increment)
-    for i in range(int(max(0, index - bubble_samples_needed)), int(min(index + bubble_samples_needed, len(proccessedRanges)))):
-        proccessedRanges[i] = 0.0
-        
-    # Pick the largest gap and choose a direction
-    target_angle = 0
+            close_distance = ranges[i]
+    bubble_index_offset = int(math.degrees(math.atan(car_width / close_distance))) * (1.0 / math.degrees(scans.angle_increment))
+    for i in range(int(max(0, index - bubble_index_offset)), int(min(index + bubble_index_offset, len(ranges)))):
+        ranges[i] = 0.0 
 
-    # Head towards the deeepest point in the gap
-    deepest_point = 0.0 
-    deepest_index = 0
-    if(close_angle >= 0.0): 
-        sublist = proccessedRanges[:index]
-        target_angle = math.atan((car_width / 2 + tolerance) / max(sublist)) 
-        deepest_point = max(sublist)
-        deepest_index = sublist.index(deepest_point)
-    else: 
-        sublist = proccessedRanges[index:]
-        target_angle = math.atan((car_width / 2 + tolerance) / max(sublist)) 
-        deepest_point = max(sublist)
-        deepest_index = index + sublist.index(deepest_point)
+def findBestGap(ranges, angle_inc):
+    gap_threshold = 1.0
+    #Two pointer solution to find all gaps within a threshold
+    gaps = []
+    l=0
+    r=1
+    while r<len(ranges):
+        if ranges[l]<gap_threshold:
+            l+=1
+            r=l+1
+        elif ranges[r]<gap_threshold:
+            neededSamples =  int(abs(math.atan((car_width / 2.0) / ranges[l + 1]))/angle_inc)
+            if r-l >= neededSamples:
+                gaps.append(ranges[l:r])
+            l=r+1
+            r=l+1
+        else:
+            r+=1
 
-    # TODO Dynamic velocity scaling
-    speed = 25
+    #returns the gap with the largest distance
+    return(sorted(gaps, key=max, reverse=True)[0])
+
+    #returns the widest gap
+    #return(sorted(gaps, key=len, reverse=True)[0])
+
+def pickGap(data):
+
+    global limit_threshold
+    global minGapDistance
+
+    # Define the indexing range for -90 to 90 degrees
+    index_min = int((np.radians(-90) - data.angle_min) / data.angle_increment)
+    index_max = int((np.radians(90) - data.angle_min) / data.angle_increment)
+    forwardRanges = data.ranges[index_min:index_max]
+
+     # Calculate the max gap and its deepest point
+    bestGap = findBestGap(forwardRanges, data.angle_increment)
+    startOfBestGap = data.ranges.index(bestGap[0])
+    indexInBestGap =  bestGap.index(max(bestGap))
+    maxGapIndex = startOfBestGap+indexInBestGap
+    max_gap = data.ranges[maxGapIndex]
+
+    # Return the appropriate angle
+    if max_gap > limit_threshold:
+        angle = (maxGapIndex * data.angle_increment + data.angle_min) * 180.0/np.pi
+    else:
+        deepest_direction_point = data.ranges.index(max(data.ranges))
+        angle = (deepest_direction_point * data.angle_increment + data.angle_min) * 180.0/np.pi
+
+    return angle
+
+def cornerDetection(data):
+
+    global car_width
     
-    # Return an angle to turn towards (0 is straight ahead)
-    return target_angle, speed 
+    # Define the indexing range for up to -90 and up to 90 degrees
+    index_min = int((np.radians(-100) - data.angle_min) / data.angle_increment)
+    index_max = int((np.radians(100) - data.angle_min) / data.angle_increment)
+
+    # if there is a value less than car_width in the range or inf or 0, return true
+    for i in range(0, index_min):
+        if data.ranges[i] < car_width / 2 and data.ranges[i] != 0:
+            print("corner detected")
+            return True
+    for i in range(index_max, len(data.ranges)):
+        if data.ranges[i] < car_width / 2 and data.ranges[i] != 0:
+            print("corner detected")
+            return True
+    
+    return False
 
 def callback(data):
-    angle, speed = findGap(data)
+
+    global car_width
+    global threshold 
+    
+    # Step 1 - Find disparities in the scan
+    ranges = extendDisparities(data)
+    new_scan = data
+    new_scan.ranges = ranges
+    postBubble = createBubble(new_scan)
+    postBubbleScan = data
+    postBubbleScan.ranges = postBubble
+    # publish disparity scan for rviz
+    disparity_pub.publish(postBubbleScan)
+
+
+    # Step 2 - Pick the largest gap in the scan, between -90 and 90 degrees
+    angle = pickGap(postBubbleScan)
+    
+    # Step 3 - Corner detection
+    #TODO: not working
+    # angle = 0 if cornerDetection(new_scan) else angle
+
+    # Step 4 - Vary the speed based on the angle
+    speed = 40.0 - (abs(angle) / 90.0) * 40.0
+
+    # Step 5 - Publish the command
     command = AckermannDrive()
     command.steering_angle = max(-100.0, min(100.0, angle))
     command.speed = max(0.0, min(40.0, speed)) 
     print("Angle: " + str(angle) + ", Speed: " + str(speed))
     command_pub.publish(command)
 
-def main():
-    rospy.init_node('follow_the_gap', anonymous=True)
-    rospy.Subscriber('/car_8/laser/scan', LaserScan, process_lidar_scan)
-    rospy.spin()
-
 if __name__ == '__main__':
-    main()
+
+    car_width = 0.2
+    threshold = 0.1
+    limit_threshold = 2.0
+    tolerance = 4
+    minGapDistance = 2.0
+
+    rospy.init_node('ftg', anonymous = True)
+    rospy.Subscriber("/car_8/scan", LaserScan, callback)
+    rospy.spin()
